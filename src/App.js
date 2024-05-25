b0796bfe8f8b2a74ace34e5a1de98930eeb1b92a
karthickow <karthickkrishnan24@gmail.com>
2024-05-26 00:41:51 +0530

    new branch feature changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>I am K karthick</h1>
+          <h1>I was changed on the feature branch</h1>
         </p>
         <a
           className="App-link"
